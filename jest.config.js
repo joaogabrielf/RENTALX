c067599363417ad103887fe5df0eecad03f162ef
JoaoG <joaogabriel@protonmail.com>
2023-04-01 12:59:29 -0300

    fix

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const config = {
+    moduleDirectories: ["node_modules"],
+    modulePaths: ["<rootDir>/dist/"],
+    roots: ["<rootDir>/dist/"],
+    verbose: true,
+};
+
+module.exports = config;
