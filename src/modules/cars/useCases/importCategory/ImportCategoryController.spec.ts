29cda8880a68155baf5d5c212d523099bbabb0da
JoaoG <joaogabriel@protonmail.com>
2023-04-02 00:51:19 -0300

    fix

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.spec.ts
@@ -0,0 +1,53 @@
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { app } from "@shared/infra/http/app";
+import { AppDataSource } from "@shared/infra/typeorm/data-source";
+import { databaseOptions } from "@shared/infra/typeorm/database";
+
+let dataSource: DataSource;
+describe("POST /import", () => {
+    beforeAll(async () => {
+        dataSource = new DataSource(databaseOptions);
+
+        await dataSource.initialize();
+        await dataSource.synchronize();
+
+        const password = await hash("admin", 8);
+        await dataSource.query(
+            `INSERT INTO USERS("name", "email", "password", "isAdmin", "driver_license")
+            SELECT 'Admin', 'admin@rentalx.com', '${password}', true, 'ASD-1243'
+            WHERE NOT EXISTS (
+            SELECT ID FROM USERS WHERE LOWER(NAME) = 'admin')`
+        );
+    });
+
+    it("should be able list all cars", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentalx.com",
+            password: "admin",
+        });
+        await request(app)
+            .post("/cars")
+            .send({
+                name: "Name Car1 Supertest",
+                description: "Desc Car1 Supertest",
+                daily_rate: 120,
+                license_plate: "QWE-1334",
+                fine_amount: 54,
+                brand: "Brand Car Supertest",
+                category_id: null,
+            })
+            .set({
+                Authorization: `Bearer ${responseToken.body.token}`,
+            });
+        expect(1 + 1).toEqual(2);
+    });
+
+    afterAll(async () => {
+        await dataSource.dropDatabase();
+        await dataSource.destroy();
+        await AppDataSource.destroy();
+    });
+});
